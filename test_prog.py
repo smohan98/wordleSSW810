d359f7b761ed5e0223bdae059756b750b23c4b65
Shreya Mohan <shreyamohan@shreyas-mbp.myfiosgateway.com>
2022-02-21 20:10:29 -0500

    tests

---
+++ test_prog.py
@@ -0,0 +1,40 @@
+
+import unittest
+import HW02_Shreya_Mohan
+import HW03_Shreya_Mohan_dictionary
+import HW03_Shreya_Mohan_ui
+
+
+class TestWordle(unittest.TestCase):
+    # def test_letter(self):
+    #     pass
+    #     # data = [1, 2, 3]
+    #     # result = sum(data)
+    #     # self.assertEqual(result, 6)
+    
+    # def test_checker(self):
+    #     pass
+
+    # def test_checker(self):
+    #     pass
+
+    # def test_checker(self):
+    #     pass
+
+    # def test_checker(self):
+    #     pass
+
+    # def test_checker(self):
+    #     pass
+
+    # def test_checker(self):
+    #     pass
+
+    # def test_checker(self):
+    #     pass
+
+    def test_checker(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
